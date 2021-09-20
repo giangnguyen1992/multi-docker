c6eea1475f8edecd9f1a0b98308eaeda71e2ebb6
giangnguyen1992 <tgn1992@hotmail.de>
2021-09-20 21:30:26 +0200

    updated travis ci

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Homeeeeeee</Link>
+          <Link to="/">Homeeeeeeeeeeee</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
